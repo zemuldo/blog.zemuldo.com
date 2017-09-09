3e66ce6828695d9cb8246161dccc8c153b573d36
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-09-09 12:47:21 +0300

    adds render login page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {Loader, Input, Menu } from 'semantic-ui-react'
+import Login from './login/loginForm'
+import 'semantic-ui-css/semantic.min.css';
 
 class App extends Component {
     state = {}
@@ -10,16 +12,7 @@ class App extends Component {
         const { activeItem } = this.state
 
         return (
-            <Menu>
-              <Loader active inline='centered' />
-              <Menu.Item header>Home</Menu.Item>
-              <Menu.Item name='About' active={activeItem === 'about'} onClick={this.handleItemClick} />
-              <Menu.Item name='Contacts' active={activeItem === 'jobs'} onClick={this.handleItemClick} />
-              <Menu.Item name='Location' active={activeItem === 'locations'} onClick={this.handleItemClick} />
-              <Menu.Item position='right'>
-                <Input className='icon' icon='search' placeholder='Search...' />
-              </Menu.Item>
-            </Menu>
+            <Login />
         )
     }
 }
