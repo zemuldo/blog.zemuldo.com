2a29a2841f33e1e2fab000b111d5b0f8627e51c4
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-09-20 17:18:20 +0300

    enforces header control

--- zemuldo.com.js
+++ zemuldo.com.js
@@ -1,11 +1,33 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+let express = require('express');
+let path = require('path');
+let  app = express();
+let bodyParser = require('body-parser');
+let helmet = require('helmet')
+let checkMe = require('cookie-session')
+let ENV = require('./config/env');
+let env = ENV().raw.NODE_ENV
+const conf = require('./config/conf')
+
+
+
+app.use(bodyParser.json());
+app.use(helmet())
+app.set('x-powered-by',false)
+app.set('X-Powered-By',false)
+app.use(helmet.ieNoOpen())
+app.use(helmet.xssFilter())
+app.use(helmet.noSniff())
+app.use(helmet({
+    frameguard: false,
+    noCache:true
+}))
 
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
-app.listen(8080);
+console.log(conf[env].httpPort)
+app.listen(conf[env].httpPort,()=>{
+    console.log("**Server started at http://localhost:"+conf[env].httpPort)
+});
