2de53c8c955d4fb4e8a3befe243e6f1f67773e53
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-09-24 13:04:00 +0300

    updates content for crawlers

--- zemuldo.com.js
+++ zemuldo.com.js
@@ -44,7 +44,29 @@ app.get("*", function (req, res) {
     let botPattern = "(googlebot\/|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)";
     let re = new RegExp(botPattern, 'i');
     if (re.test(res.locals.ua)) {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+        let html = '<!doctype html>\n' +
+            '<html lang="en">\n' +
+            '<head>\n' +
+            '    <meta charset="utf-8">\n' +
+            '    <meta name="author" content="Danstan Otieno Onyango">\n' +
+            '    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">\n' +
+            '    <link rel="shortcut icon" href="img/icons/NN.png">\n' +
+            '    <link rel="stylesheet" type="text/css" href="./semantic.min.css" />\n' +
+            '    <link rel="stylesheet" type="text/css" href="./styles.css" />\n' +
+            '    <meta name="description" content="Best Place for your reading, Tech and Business articles. Here you find featured content on current technologies like BigData, ML, AI, Deep Learning, DataScience and more">\n' +
+            '    <meta name="keywords" content="Tech Articles, Business Articles, Blogs, BigData, Artificila Intelligence, Marchine Learning, Business Development">\n' +
+            '    <title>ZemuldO.COM</title>\n' +
+            '</head>\n' +
+            '<body>\n' +
+            '<p>\n' +
+            '    The body content will go here\n' +
+            '</p>\n' +
+            '<div id="root"></div>\n' +
+            '<!--This is the main entry point to this project-->\n' +
+            '</body>\n' +
+            '\n' +
+            '</html>\n'
+        res.send(html);
         console.log('the user agent is a crawler!');
     }
     else {
