4e1e5940b1a1e948e72b2b6252966334940c023c
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-09-28 22:10:27 +0300

    adds render editor after login

--- src/login/loginForm.js
+++ src/login/loginForm.js
@@ -1,50 +1,89 @@
 import React, {Component} from 'react'
 import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import RichEditorExample from '../partials/createBlog'
 /*import config from '../environments/conf'
 const env = config[process.env.NODE_ENV] || 'development'*/
 class LoginForm extends Component {
     constructor(props){
         super(props);
         this.state = {
-           user:null
+           user:null,
+            loggedin:false,
+            password:null,
+            userName:null
         };
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleUnamedChange = this.handleUnamedChange.bind(this);
+        this.onLoginClick = this.onLoginClick.bind(this);
     };
-  render(){
-    return(  <div className='login-form'>
-          <Grid
-              textAlign='center'
-              style={{ height: '100%',marginTop:25 }}
-              verticalAlign='middle'
-          >
-            <Grid.Column style={{ maxWidth: 300 }}>
-              <Header as='h2' color='teal' textAlign='center'>
-                <Image src='/img/login/login.png' />
-              </Header>
-              <Form size='large'>
-                <Segment stacked>
-                  <Form.Input
-                      fluid
-                      icon='user'
-                      iconPosition='left'
-                      placeholder='E-mail address'
-                  />
-                  <Form.Input
-                      fluid
-                      icon='lock'
-                      iconPosition='left'
-                      placeholder='Password'
-                      type='password'
-                  />
+    onLoginClick= () => {
+        if(this.state.userName==='zemuldo' && this.state.password==='omera'){
+            this.setState({loggedin:true})
+        }
+        else {
+            this.setState({loggedin:true})
+        }
 
-                  <Button color={this.props.color} fluid size='large'>Login</Button>
-                </Segment>
-              </Form>
-              <Message>
-                New to us?  <a href="/"> Sign Up</a>
-              </Message>
-            </Grid.Column>
-          </Grid>
+    }
+
+    handlePasswordChange(e) {
+        e.preventDefault();
+        this.setState({password:e.target.value})
+
+    }
+    handleUnamedChange(e) {
+        e.preventDefault();
+        this.setState({userName:e.target.value})
+
+    }
+  render(){
+    return(
+        <div>
+            {
+                this.state.loggedin?
+                    <div>
+                        <RichEditorExample/>
+                </div>:
+                    <div className='login-form'>
+                        <Grid
+                            textAlign='center'
+                            style={{ height: '100%',marginTop:25 }}
+                            verticalAlign='middle'
+                        >
+                            <Grid.Column style={{ maxWidth: 300 }}>
+                                <Header as='h2' color='teal' textAlign='center'>
+                                    <Image src='/img/login/login.png' />
+                                </Header>
+                                <Form size='large'>
+                                    <Segment stacked>
+                                        <Form.Input
+                                            fluid
+                                            icon='user'
+                                            iconPosition='left'
+                                            placeholder='E-mail address'
+                                            onChange={this.handleUnamedChange}
+                                        />
+                                        <Form.Input
+                                            fluid
+                                            icon='lock'
+                                            iconPosition='left'
+                                            placeholder='Password'
+                                            type='password'
+                                            onChange={this.handlePasswordChange}
+                                        />
+
+                                        <Button  type="button" onClick={this.onLoginClick}  color={this.props.color} fluid size='large'>Login</Button>
+                                    </Segment>
+                                </Form>
+                                <Message>
+                                    New to us?  <a href="/"> Sign Up</a>
+                                </Message>
+                            </Grid.Column>
+                        </Grid>
+                    </div>
+            }
         </div>
+
     )
   }
 }
