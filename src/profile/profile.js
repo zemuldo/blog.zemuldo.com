bc168aec914cee45d5e48bfce900cf5805aab170
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-10-12 07:30:13 +0300

    adds load users profile previous session and blogs

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -20,18 +20,27 @@ class RichEditorExample extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            user:this.props.currentUser,
+            user:JSON.parse(localStorage.getItem('user')),
             createNew:false,
             blogs:null,
             blog:null,
             blogsLoaded:false,
+            blogDetails:null,
             blogLoaded:false,
             blogIsLoading:false,
+            counts:{
+                fbC:null,
+                twtC:null,
+                gplsC:null
+            },
+            richViewerState:null
         };
         this.componentDidMount=this.componentDidMount.bind(this)
         this.componentWillUnmount=this.componentWillUnmount.bind(this)
         this.blogsAreLoading = this.blogsAreLoading.bind(this);
         this.blogIsLoading = this.blogIsLoading.bind(this);
+        this.onReadMore = this.onReadMore.bind(this);
+        this.setBlogCounts = this.setBlogCounts.bind(this);
 
     }
     blogsAreLoading(state){
@@ -39,6 +48,30 @@ class RichEditorExample extends React.Component {
     }
     blogIsLoading(state){
         this.setState({blogLoaded:!state})
+    }
+    setCurrentBlog(thisBlog){
+        return axios.post(env.httpURL, {
+            "query":"getPost",
+            "queryParam":{
+                id:thisBlog.id
+            }
+        })
+            .then(response => {
+                if(response.data.error){
+                }
+                else {
+                    this.setState({blog:response.data,blogDetails:thisBlog})
+                    this.setState({blogIsLoading:false,richViewerState:response.data.body})
+                    this.blogIsLoading(false)
+                    window.scrollTo(0,0)
+                }
+
+            })
+            .catch(exception => {
+                this.setState({blog:null})
+                this.blogIsLoading(false)
+                return exception
+            });
     }
     resize = () => this.forceUpdate();
     componentDidMount() {
@@ -69,6 +102,65 @@ class RichEditorExample extends React.Component {
     }
     componentWillUnmount() {
         window.removeEventListener('resize', this.resize)
+    }
+    setBlogCounts(){
+        let gplusPost = {
+            "method": "pos.plusones.get",
+            "id": "p",
+            "params": {
+                "nolog": true,
+                "id": "https://zemuldo/"+this.state.blogDetails.title.split(' ').join('-')+'_'+this.state.blogDetails.date.split(' ').join('-')+'_'+this.state.blogDetails.id.toString(),
+                "source": "widget",
+                "userId": "@viewer",
+                "groupId": "@self"
+            },
+            "jsonrpc": "2.0",
+            "key": "p",
+            "apiVersion": "v1"
+        }
+        window.scrollTo(0,0);
+        return Promise.all([
+            axios.get('https://graph.facebook.com/?id=http://zemuldo.com/'+this.state.blogDetails.title.split(' ').join('%2520')+'_'+this.state.blogDetails.date.split(' ').join('%2520')+'_'+this.state.blogDetails.id.toString(),{}),
+            axios.get('http://public.newsharecounts.com/count.json?url=http://zemuldo.com/'+this.state.blogDetails.title.split(' ').join('-')+'_'+this.state.blogDetails.date.split(' ').join('-')+'_'+this.state.blogDetails.id.toString(),{}),
+            axios.post(' https://clients6.google.com/rpc',gplusPost),
+        ])
+            .then(function (res) {
+                this.setState({counts:{
+                    fbC:(res[0].data.share.share_count)? res[0].data.share.share_count:0,
+                    twtC:(res[1].data.count)?res[1].data.count:0,
+                    gplsC:(res[2].data.result.metadata.globalCounts.count)?res[2].data.result.metadata.globalCounts.count:0
+                }})
+            }.bind(this))
+            .catch(function (err) {
+                this.setState({counts:{
+                    fbC:0,
+                    twtC:0,
+                    gplsC:0
+                }})
+            }.bind(this))
+    }
+    onReadMore(thisBlog){
+        this.setState({blogIsLoading:true});
+        return axios.post(env.httpURL, {
+            "queryMethod":"getPost",
+            "queryData":{
+                "id":thisBlog.id
+            }
+        })
+            .then(response => {
+                this.setState({blog:response.data,blogDetails:thisBlog});
+                this.setState({blogIsLoading:false,richViewerState:response.data.body});
+                window.scrollTo(0,0);
+                this.setCurrentBlog(thisBlog)
+                this.setBlogCounts()
+            })
+            .catch(function (err) {
+                this.setState({blog:null,blogDetails:thisBlog});
+                this.setState({blogIsLoading:false});
+                window.scrollTo(0,0);
+                return err;
+            }.bind(this))
+
     }
     render() {
         return (
@@ -131,7 +223,14 @@ class RichEditorExample extends React.Component {
                                                 handleUTAChange={this.handleUTAChange}
                                                 handleCategoryChange={this.handleCategoryChange}
                                                 handleTopicChange={this.handleTopicChange} />:
-                                            <Welcome color={this.props.color} blog = {null}/>
+                                            <Welcome
+                                                richViewerState={this.state.richViewerState}
+                                                counts={this.state.counts}
+                                                color={this.props.colors[1]}
+                                                blogDetails={this.state.blogDetails}
+                                                blog={this.state.blog}
+                                                blogs={this.state.blogs}
+                                                blogLoaded={this.state.blogLoaded}/>
                                     }
                                 </Grid.Column>:
                                 <Grid.Column  width={16}>
@@ -146,7 +245,14 @@ class RichEditorExample extends React.Component {
                                                 handleUTAChange={this.handleUTAChange}
                                                 handleCategoryChange={this.handleCategoryChange}
                                                 handleTopicChange={this.handleTopicChange} />:
-                                            <Welcome color={this.props.color} blog = {null}/>
+                                            <Welcome
+                                                richViewerState={this.state.richViewerState}
+                                                counts={this.state.counts}
+                                                color={this.props.colors[1]}
+                                                blogDetails={this.state.blogDetails}
+                                                blog={this.state.blog}
+                                                blogs={this.state.blogs}
+                                                blogLoaded={this.state.blogLoaded}/>
                                     }
                                 </Grid.Column>
                         }
