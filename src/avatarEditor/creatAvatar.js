2753d43afb32b3393c598b225521cb8a38d4de74
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-10-13 01:33:25 +0300

    adds function to convert image base64 to blob

--- src/avatarEditor/creatAvatar.js
+++ src/avatarEditor/creatAvatar.js
@@ -2,6 +2,26 @@ import React from 'react'
 import ReactAvatarEditor from './avatarEditor'
 import { Button,Grid} from 'semantic-ui-react'
 
+function dataURItoBlob(dataURI, callback) {
+    // convert base64 to raw binary data held in a string
+    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+    let byteString = atob(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    let mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+    // write the bytes of the string to an ArrayBuffer
+    let ab = new ArrayBuffer(byteString.length);
+    let ia = new Uint8Array(ab);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    // write the ArrayBuffer to a blob, and you're done
+    let bb = new Blob([ab]);
+    console.log(bb)
+    return bb;
+}
 class AvaratEditor extends React.Component {
     constructor(props){
         super(props)
@@ -24,6 +44,7 @@ class AvaratEditor extends React.Component {
     handleSave = data => {
         const img = this.editor.getImageScaledToCanvas().toDataURL()
         const rect = this.editor.getCroppingRect()
+        dataURItoBlob(img)
 
         this.setState({
             preview: {
