dae57e31ed7797621f8b1a7f960d3489f30b7b45
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-10-14 16:18:20 +0300

    removes like button from bvlogs

--- src/posts/blogs.js
+++ src/posts/blogs.js
@@ -9,28 +9,13 @@ class Blogs extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            
         };
         this.componentDidMount = this.componentDidMount.bind(this)
-        this.updateLikes=this.updateLikes.bind(this)
 
     };
     componentDidMount() {
     }
-    updateLikes=(id)=>{
-        return axios.post(env.httpURL, {
-            "queryMethod":"updateBlogLikes",
-            "queryData":{
-                "id":id
-            }
-        })
-            .then(function (response) {
 
-            }.bind(this))
-            .catch(function (err) {
-
-            }.bind(this));
-    }
     render() {
         return (
             <div>
@@ -40,9 +25,31 @@ class Blogs extends React.Component {
                             { _.times(this.props.blogs.length, i =>
                                     <List.Item key={this.props.blogs[i]._id} >
                                         <List.Icon name='leaf' />
-                                        <List.Content><Header color='green' as='h3'>{this.props.blogs[i].title.split(' ').join(' ')}</Header></List.Content>
-                                        <List.Content>Author: {this.props.blogs[i].author}</List.Content><span><button><Icon color='green' name="thumbs up"/></button><span><i style={{color:'orange'}}>~{this.props.blogs[i].likes}</i></span></span>
-                                        <button className="redMoreButton" ref={this.props.blogs[i]._id} onClick={() => { this.props.onReadMore(this.props.blogs[i]) }} name="all" style={{color:'blue',backgroundColor:'transparent',border:'none'}}><span>Read</span></button>
+                                        <List.Content>
+                                            <Header color='green' as='h3'>
+                                                {this.props.blogs[i].title.split(' ').join(' ')}
+                                                </Header>
+                                        </List.Content>
+                                        <List.Content>
+                                            Author: {this.props.blogs[i].author}
+                                            </List.Content>
+                                        <span>
+                                            Likes:
+                                            <span>
+                                                <i style={{color:'orange'}}>
+                                                ~{this.props.blogs[i].likes}
+                                                </i>
+                                            </span>
+                                        </span>
+                                        <button
+                                            className="redMoreButton"
+                                            ref={this.props.blogs[i]._id}
+                                            onClick={() => { this.props.onReadMore(this.props.blogs[i]) }}
+                                            name="all"
+                                            style={{color:'blue',backgroundColor:'transparent',border:'none'}}
+                                        >
+                                            <span>Read</span>
+                                        </button>
                                         <hr/>
                                     </List.Item>
                                 )
@@ -62,15 +69,22 @@ class Blogs extends React.Component {
                                         <List.Content>
                                             Author: {this.props.blogs[i].author}
                                             </List.Content>
-                                        <button onClick={()=>this.updateLikes(this.props.blogs[i].id)} className="clearElem">
-                                            <Icon color='green' name="thumbs up"/>
-                                        </button>
+                                        <span >
+                                            Likes:
+                                        </span>
                                             <span>
                                                 <i style={{color:'orange'}}>
                                                     ~{this.props.blogs[i].likes}
                                                 </i>
                                             </span>
-                                        <button className="redMoreButton" ref={this.props.blogs[i]._id} onClick={() => { this.props.onReadMore(this.props.blogs[i]) }} name="all" style={{color:'blue',backgroundColor:'transparent',border:'none'}}><span>Read</span></button>
+                                        <button
+                                            className="redMoreButton"
+                                            ref={this.props.blogs[i]._id}
+                                            onClick={() => { this.props.onReadMore(this.props.blogs[i]) }}
+                                            name="all" style={{color:'blue',backgroundColor:'transparent',border:'none'}}
+                                        >
+                                            <span>Read</span>
+                                        </button>
                                         <hr/>
                                     </List.Item>
                                 )
