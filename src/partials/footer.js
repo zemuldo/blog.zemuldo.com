0a79f3ba1134a1ae59cd25c3de7e884fdf47231a
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-10-27 23:12:13 +0300

    updates foote

--- src/partials/footer.js
+++ src/partials/footer.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import {  Menu, Button,Icon,Header,Modal} from 'semantic-ui-react'
+import {  Menu, Button,Icon,Header,Modal,List,Grid,Divider,Container,Segment,Image} from 'semantic-ui-react'
 import ReviewPortal from './portal'
-/*import config from '../environments/conf'
-const env = config[process.env.NODE_ENV] || 'development'*/
+import config from '../environments/conf'
+const env = config[process.env.NODE_ENV] || 'development'
 class Footer extends Component {
     constructor(props){
         super(props);
@@ -18,82 +18,134 @@ class Footer extends Component {
     render() {
         return (
             <div>
-                {
-                    (window.innerWidth>800) ?
-                        <Menu compact={true} style={{position: 'fixed',bottom: '0%',right: '20%'}} fixed='bottom' pointing size='tiny' color="green" borderless>
-                            <Menu.Item  name='ZemuldO-Home' active={this.props.current === 'ZemuldO-Home'} onClick={this.handleHomeClick}>
-                                <a href="/" rel="noreferrer noopener">
-                                    <Icon color={this.props.color} name='home' />
-                                    <span color={this.props.color}>Home</span>
-                                </a>
-                            </Menu.Item>
-                            <Menu.Item name='copyright' onClick={this.handleMenuItemClick}>
-                                <a href="https://danstan.zemuldo.com" rel="noreferrer noopener" target="_blank">
-                                    <Icon color={this.props.color} name='copyright' />
-                                    <span color={this.props.color}>CopyRight {" Zemuldo "+new Date().getFullYear()}</span>
-                                </a>
+                <Segment
+                    vertical
+                    style={{margin: '0em 0em 0em 0em', padding: '0em 0em 0em 0em' }}
+                >
+                    <Container textAlign='center'>
+                        <Grid divided stackable>
+                            <Grid.Row>
+                                <Grid.Column width={3}>
 
-                            </Menu.Item>
-                            <Modal
-                                trigger={
-                                    <Menu.Item name='Cookie Policy' onClick={this.handleCookieOpen} >
-                                        <Icon color={this.props.color} name='privacy' />
-                                        <span color={this.props.color}>Cookie Policy</span>
-                                    </Menu.Item>
-                                }
-                                open={this.state.modalOpen}
-                                onClose={this.handleCookieClose}
-                                basic
-                                size='small'
-                            >
-                                <Header icon='browser' content='Cookies policy' />
-                                <Modal.Content>
-                                    <h3>This website uses cookies to ensure the best user experience.</h3>
-                                    <p>Cookies help you have better experience. The full Policy is here
-                                        <a href="https://policy.zemuldo.com/cookie" rel="noreferrer noopener" target="_blank">
-                                            <Icon color={this.props.color} name='privacy' />
-                                            <span color={this.props.color}>Zemuldo.com Cookie Policy</span>
+                                </Grid.Column>
+                                <Grid.Column width={3}>
+                                    <Header color='green' as='h4' content='Socila Profiles' />
+                                    <List link>
+                                        <List.Item>
+                                            <a href="https://github.com/zemuldo" rel="noreferrer noopener" target="_blank">
+                                                <Icon color='black' name='github' />
+                                                <span color={this.props.color}>GitHub</span>
+                                            </a>
+                                        </List.Item>
+                                        <List.Item >
+                                            <a href="https://twitter.com/zemuldo" rel="noreferrer noopener" target="_blank">
+                                                <Icon color='blue' name='twitter' />
+                                                <span color={this.props.color} >Twitter</span>
+                                            </a>
+                                        </List.Item>
+                                        <List.Item>
+                                            <a href="https://facebook.com/zemuldo" rel="noreferrer noopener" target="_blank">
+                                                <Icon color='blue' name='facebook' />
+                                                <span color={this.props.color}>GitHub</span>
+                                            </a>
+                                        </List.Item>
+                                    </List>
+                                </Grid.Column>
+                                <Grid.Column width={3}>
+                                    <Header color='green' inverted as='h4' content='Navigation' />
+                                    <List link>
+                                        <List.Item>
+                                            <a onClick={()=>this.props.handleHomeClick()}>
+                                                <Icon color='blue' name='home' />
+                                                <span color={this.props.color}>Home</span>
+                                            </a>
+                                        </List.Item>
+                                        <List.Item>
+                                            <a onClick={()=>this.props.handleMenuItemClickFooter('tuts')}>
+                                                <Icon color='blue' name='code' />
+                                                <span color={this.props.color}>Tutorials</span>
+                                            </a>
+                                        </List.Item>
+                                        <List.Item>
+                                            <a onClick={()=>this.props.handleMenuItemClickFooter('tech')}>
+                                                <Icon color='blue' name='server' />
+                                                <span color={this.props.color}>Tech</span>
+                                            </a>
+                                        </List.Item>
+                                        <List.Item>
+                                            <a onClick={()=>this.props.handleMenuItemClickFooter('business')}>
+                                                <Icon color='blue' name='book' />
+                                                <span color={this.props.color}>Business</span>
+                                            </a>
+                                        </List.Item>
+                                        <List.Item>
+                                        <a href="https://danstan.zemuldo.com" rel="noreferrer noopener" target="_blank">
+                                            <Icon color='blue' name='info' />
+                                            <span color={this.props.color}>About</span>
                                         </a>
-                                    </p>
-                                </Modal.Content>
-                                <Modal.Actions>
-                                    <Button color={this.props.color} onClick={this.handleCookieClose} inverted>
-                                        <Icon name='checkmark' /> OK
-                                    </Button>
-                                </Modal.Actions>
-                            </Modal>
-                            <Menu.Item name='github' onClick={this.handleMenuItemClick}>
-                                <a href="https://github.com/zemuldo" rel="noreferrer noopener" target="_blank">
-                                    <Icon color='black' name='github' />
-                                    <span color={this.props.color}>GitHub</span>
-                                </a>
-                            </Menu.Item>
-                            <Menu.Item name='twitter' onClick={this.handleMenuItemClick}>
-                                <a href="https://twitter.com/zemuldo" rel="noreferrer noopener" target="_blank">
-                                    <Icon color='blue' name='twitter' />
-                                    <span color={this.props.color} >Twitter</span>
-                                </a>
-                            </Menu.Item>
-                            <ReviewPortal/>
-                        </Menu>
-                        :
-                        <Menu style={{margin:'0em 30em 0em 30em'}} fixed='bottom' pointing size='tiny' color="green" borderless>
-                            <Menu.Item  name='ZemuldO-Home' active={this.props.current === 'ZemuldO-Home'} onClick={this.handleHomeClick}>
-                                <a href="/" rel="noreferrer noopener">
-                                    <Icon color={this.props.color} name='home' />
-                                    <span color={this.props.color}>Home</span>
-                                </a>
-                            </Menu.Item>
-                            <Menu.Item name='copyright' onClick={this.handleMenuItemClick}>
+                                    </List.Item>
+
+                                    </List>
+                                </Grid.Column>
+                                <Grid.Column width={3}>
+                                    <Header color='green' as='h4' content='Quote of the day' />
+                                    <p>Every complex System no matter how Complex is a combination of many smaller and simpler Systems and Units that operate together Synchronously</p>
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                        <br/>
+                        <hr color="green"/>
+                        <br/>
+                        <Image
+                            centered
+                            size='mini'
+                            src='/logo.png'
+                        />
+                        <List horizontal  divided link>
+                            <List.Item as='a' href={env.httpURL+'/sitemap.xml'} rel="noreferrer noopener" target="_blank">Site Map</List.Item>
+                            <List.Item>Contact Us</List.Item>
+                            <List.Item>Terms and Conditions</List.Item>
+                            <List.Item>
+                                <Modal
+                                    trigger={
+                                        <Menu.Item name='Cookie Policy' onClick={this.handleCookieOpen} >
+                                            <Icon color={this.props.color} name='privacy' />
+                                            <span color={this.props.color}>Cookie Policy</span>
+                                        </Menu.Item>
+                                    }
+                                    open={this.state.modalOpen}
+                                    onClose={this.handleCookieClose}
+                                    basic
+                                    size='small'
+                                >
+                                    <Header icon='browser' content='Cookies policy' />
+                                    <Modal.Content>
+                                        <h3>This website uses cookies to ensure the best user experience.</h3>
+                                        <p>Cookies help you have better experience. The full Policy is here
+                                            <a href="https://policy.zemuldo.com/cookie" rel="noreferrer noopener" target="_blank">
+                                                <Icon color={this.props.color} name='privacy' />
+                                                <span color={this.props.color}>Zemuldo.com Cookie Policy</span>
+                                            </a>
+                                        </p>
+                                    </Modal.Content>
+                                    <Modal.Actions>
+                                        <Button color={this.props.color} onClick={this.handleCookieClose} inverted>
+                                            <Icon name='checkmark' /> OK
+                                        </Button>
+                                    </Modal.Actions>
+                                </Modal>
+                            </List.Item>
+                            <List.Item>
                                 <a href="https://danstan.zemuldo.com" rel="noreferrer noopener" target="_blank">
                                     <Icon color={this.props.color} name='copyright' />
                                     <span color={this.props.color}>CopyRight {" Zemuldo "+new Date().getFullYear()}</span>
                                 </a>
-
-                            </Menu.Item>
-                        </Menu>
-
-                }
+                            </List.Item>
+                        </List>
+                    </Container>
+                    <br/>
+                </Segment>
+                <ReviewPortal/>
             </div>
         )
     }
