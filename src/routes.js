454ba857cf4c9fa36a4f5e12965123faf20ebf90
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-11-08 09:46:35 +0300

    adds routes

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Login from './profile/loginForm'
+import Page from './pages/index'
+import {  Switch, Route } from 'react-router-dom'
+
+class Routes extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+        };
+    };
+    render() {
+        return (
+            <main>
+                <Switch>
+                    <Route exact path='/' component={Page}/>
+                    <Route path='/dev' component={Page}/>
+                    <Route path='/tutorials' component={Page}/>
+                    <Route path='/tech' component={Page}/>
+                    <Route path='/business' component={Page}/>
+                    <Route path='/reviews' component={Page}/>
+                    <Route path='/tutorials' component={Page}/>
+                    <Route path='/login' component={Login}/>
+                </Switch>
+            </main>
+        )
+    }
+}
+export default Routes;
