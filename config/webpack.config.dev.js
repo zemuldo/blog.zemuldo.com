71c74b5eb0d7c688fa9ed2acb14ff4ba20a4c25c
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-11-23 16:08:30 +0300

    changes name of build files to hash

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -69,9 +69,9 @@ module.exports = {
     // This does not produce a real file. It's just the virtual path that is
     // served by WebpackDevServer in development. This is the JS bundle
     // containing code from all our entry points, and the Webpack runtime.
-    filename:'static/js/main.app.js',
+    filename:'static/js/main.[hash:8].js',
     // There are also additional JS chunk files if you use code splitting.
-    chunkFilename:'static/js/[name].chunk.js',
+    chunkFilename:'static/js/[name].[hash:8].chunk.js',
     // This is the URL that app is served from. We use "/" in development.
     publicPath: publicPath,
     // Point sourcemap entries to original disk location
