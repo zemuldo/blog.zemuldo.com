4f183d7aacd83e66468a83febbaec21c3d1d462a
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-11-23 18:32:25 +0300

    adds exported pages object

--- src/environments/conf.js
+++ src/environments/conf.js
@@ -1,4 +1,46 @@
 module.exports = {
+    pages:{
+        dev:{
+            name:'Development',
+            icon:'code',
+            topTitle:"Dev articles"
+        },
+        tech:{
+            name:'Technology',
+            icon:'server',
+            topTitle:" Featured in Technology"
+        },
+        business:{
+            name:'Business',
+            icon:'creative commons',
+            topTitle:" Popular in Bsuness"
+        },
+        reviews:{
+            name:'Reviews',
+            icon:'circle notched'
+        },
+        tuts:{
+            name:'Tutorials',
+            icon:'code',
+            topTitle:" Popular Tutorials"
+        },
+        home:{
+            name:'Home',
+            icon:'home'
+        },
+        profile:{
+            name:'Profile',
+            icon:'user circle'
+        },
+        about:{
+            name:'About',
+            icon:'info'
+        },
+        login:{
+            name:'Login',
+            icon:'lock'
+        }
+    },
     live:{
         httpPort:8080,
         wsURL:'ws://zemuldo.com:8090',
