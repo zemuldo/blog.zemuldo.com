12e4dd95f5df65eddf89af1efffb6c082700bef3
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-12-02 14:27:13 +0300

    testing service workers

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,24 +1,15 @@
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-     http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
+"use strict";
 
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
-
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
+function setOfCachedUrls(e) {
+    return e.keys().then(function(e) {
+        return e.map(function(e) {
+            return e.url
+        })
+    }).then(function(e) {
+        return new Set(e)
+    })
+}
+var precacheConfig = [
     "index.html",
     "static/css/main.zemuldo.css",
     "static/js/main.zemuldo.js",
@@ -28,52 +19,97 @@ const PRECACHE_URLS = [
     "static/media/icons.zemuldo.ttf",
     "static/media/icons.zemuldo.woff",
     "static/media/icons.zemuldo.woff2",
-];
-
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(PRECACHE)
-            .then(cache => cache.addAll(PRECACHE_URLS))
-            .then(self.skipWaiting())
-    );
-});
-
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-        }).then(cachesToDelete => {
-            return Promise.all(cachesToDelete.map(cacheToDelete => {
-                return caches.delete(cacheToDelete);
-            }));
-        }).then(() => self.clients.claim())
-    );
-});
-
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-    // Skip cross-origin requests, like those for Google Analytics.
-    if (event.request.url.startsWith(self.location.origin)) {
-        event.respondWith(
-            caches.match(event.request).then(cachedResponse => {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
-
-                return caches.open(RUNTIME).then(cache => {
-                    return fetch(event.request).then(response => {
-                        // Put a copy of the response in the runtime cache.
-                        return cache.put(event.request, response.clone()).then(() => {
-                            return response;
-                        });
-                    });
-                });
+],
+    cacheName = "sw-precache-v3-sw-precache-webpack-plugin-" + (self.registration ? self.registration.scope : ""),
+    ignoreUrlParametersMatching = [/^utm_/],
+    addDirectoryIndex = function(e, t) {
+        var n = new URL(e);
+        return "/" === n.pathname.slice(-1) && (n.pathname += t), n.toString()
+    },
+    cleanResponse = function(e) {
+        return e.redirected ? ("body" in e ? Promise.resolve(e.body) : e.blob()).then(function(t) {
+            return new Response(t, {
+                headers: e.headers,
+                status: e.status,
+                statusText: e.statusText
             })
-        );
+        }) : Promise.resolve(e)
+    },
+    createCacheKey = function(e, t, n, r) {
+        var a = new URL(e);
+        return r && a.pathname.match(r) || (a.search += (a.search ? "&" : "") + encodeURIComponent(t) + "=" + encodeURIComponent(n)), a.toString()
+    },
+    isPathWhitelisted = function(e, t) {
+        if (0 === e.length) return !0;
+        var n = new URL(t).pathname;
+        return e.some(function(e) {
+            return n.match(e)
+        })
+    },
+    stripIgnoredUrlParameters = function(e, t) {
+        var n = new URL(e);
+        return n.hash = "", n.search = n.search.slice(1).split("&").map(function(e) {
+            return e.split("=")
+        }).filter(function(e) {
+            return t.every(function(t) {
+                return !t.test(e[0])
+            })
+        }).map(function(e) {
+            return e.join("=")
+        }).join("&"), n.toString()
+    },
+    hashParamName = "_sw-precache",
+    urlsToCacheKeys = new Map(precacheConfig.map(function(e) {
+        var t = e[0],
+            n = e[1],
+            r = new URL(t, self.location),
+            a = createCacheKey(r, hashParamName, n, /\.\w{8}\./);
+        return [r.toString(), a]
+    }));
+self.addEventListener('install', function(event) {
+    // Put `offline.html` page into cache
+    var offlineRequest = new Request('offline.html');
+    event.waitUntil(
+        fetch(offlineRequest).then(function(response) {
+            return caches.open('offline')
+                .then(function(cache) {
+                return cache.put(offlineRequest, response);
+            })
+                .catch(function () {
+
+                })
+
+        })
+    );
+}), self.addEventListener("activate", function(e) {
+    var t = new Set(urlsToCacheKeys.values());
+    e.waitUntil(caches.open(cacheName).then(function(e) {
+        return e.keys().then(function(n) {
+            return Promise.all(n.map(function(n) {
+                if (!t.has(n.url)) return e.delete(n)
+            }))
+        })
+    }).then(function() {
+        return self.clients.claim()
+    }).catch(function () {
+
+    }))
+}), self.addEventListener("fetch", function(e) {
+    if ("GET" === e.request.method) {
+        var t, n = stripIgnoredUrlParameters(e.request.url, ignoreUrlParametersMatching);
+        t = urlsToCacheKeys.has(n);
+        t || (n = addDirectoryIndex(n, "index.html"), t = urlsToCacheKeys.has(n));
+        !t && "navigate" === e.request.mode && isPathWhitelisted(["^(?!\\/__).*"], e.request.url) && (n = new URL("https://zemuldo.co/index.html", self.location).toString(), t = urlsToCacheKeys.has(n)), t && e.respondWith(caches.open(cacheName).then(function(e) {
+            return e.match(urlsToCacheKeys.get(n))
+                .then(function(e) {
+                if (e) return e;
+                throw Error("The cached response that was expected is missing.")
+            })
+                .catch(function () {
+
+                })
+        }).catch(function(t) {
+            return console.warn('Couldn\'t serve response for "%s" from cache: %O', e.request.url, t), fetch(e.request)
+        }))
     }
 });
