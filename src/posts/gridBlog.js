ab1dbc2ec93cf02e1e0e4f1e9fae23db5d6fac0e
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-12-04 08:54:59 +0300

    redesigning grid blogs

---
+++ src/posts/gridBlog.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {Grid,Button,Header} from 'semantic-ui-react'
+
+class GridBlog extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <Grid.Column width={5}>
+                <Header color='green' as='h3'>
+                    {this.props.blog.title.split(' ').join(' ')}
+                </Header>
+                <p>By: Author: {this.props.blog.author}</p>
+                Likes:
+                <span>
+                                                <i style={{color:'orange'}}>
+                                                ~{this.props.blog.likes}
+                                                </i>
+                                            </span>
+                <hr color={this.props.color}/>
+
+                <p>{this.props.blog.about}</p>
+                <Button
+                    className="redMoreButton"
+                    ref={this.props.blog._id}
+                    onClick={() => { this.props.onReadMore(this.props.blog) }}
+                    name="all"
+                    style={{color:'blue',backgroundColor:'transparent',border:'none'}}
+                >
+                    <span>Read</span>
+                </Button>
+            </Grid.Column>
+        )
+    }
+}
+
+export default GridBlog
--- src/posts/gridBlogs.js
+++ src/posts/gridBlogs.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Loader,Grid,Button,Header} from 'semantic-ui-react'
+import GridBlog from "./gridBlog";
 
 class GridBlogs extends React.Component {
     constructor(props){
@@ -17,119 +18,89 @@ class GridBlogs extends React.Component {
                             <Grid>
                                 {
                                     this.props.blogs[0] ?
-                                        <Grid.Column width={5}>
-                                            <Header color='green' as='h3'>
-                                                {this.props.blogs[0].title.split(' ').join(' ')}
-                                            </Header>
-                                            <p>By: Author: {this.props.blogs[0].author}</p>
-                                            <p>{this.props.blogs[0].about}</p>
-                                            <Button
-                                                className="redMoreButton"
-                                                ref={this.props.blogs[0]._id}
-                                                onClick={() => { this.props.onReadMore(this.props.blogs[0]) }}
-                                                name="all"
-                                                style={{color:'blue',backgroundColor:'transparent',border:'none'}}
-                                            >
-                                                <span>Read</span>
-                                            </Button>
-                                        </Grid.Column> : null
+                                        <GridBlog
+                                            onReadMore = {this.props.onReadMore}
+                                            color={this.props.color}
+                                            blog={this.props.blogs[0]}
+                                        />:null
                                 }
                                 {
                                     this.props.blogs[1] ?
-                                        <Grid.Column width={5}>
-                                            <Header color='green' as='h3'>
-                                                {this.props.blogs[1].title.split(' ').join(' ')}
-                                            </Header>
-                                            <p>By: Author: {this.props.blogs[1].author}</p>
-                                            <p>{this.props.blogs[1].about}</p>
-                                            <Button
-                                                className="redMoreButton"
-                                                ref={this.props.blogs[1]._id}
-                                                onClick={() => { this.props.onReadMore(this.props.blogs[1]) }}
-                                                name="all"
-                                                style={{color:'blue',backgroundColor:'transparent',border:'none'}}
-                                            >
-                                                <span>Read</span>
-                                            </Button>
-                                        </Grid.Column> : null
+                                        <GridBlog
+                                            onReadMore = {this.props.onReadMore}
+                                            color={this.props.color}
+                                            blog={this.props.blogs[1]}
+                                        />:null
                                 }
                                 {
                                     this.props.blogs[2] ?
-                                        <Grid.Column width={5}>
-                                            <Header color='green' as='h3'>
-                                                {this.props.blogs[2].title.split(' ').join(' ')}
-                                            </Header>
-                                            <p>By: Author: {this.props.blogs[2].author}</p>
-                                            <p>{this.props.blogs[2].about}</p>
-                                            <Button
-                                                className="redMoreButton"
-                                                ref={this.props.blogs[2]._id}
-                                                onClick={() => { this.props.onReadMore(this.props.blogs[2]) }}
-                                                name="all"
-                                                style={{color:'blue',backgroundColor:'transparent',border:'none'}}
-                                            >
-                                                <span>Read</span>
-                                            </Button>
-                                        </Grid.Column> : null
+                                        <GridBlog
+                                            onReadMore = {this.props.onReadMore}
+                                            color={this.props.color}
+                                            blog={this.props.blogs[2]}
+                                        />:null
                                 }
                             </Grid>
                             <Grid>
                                 {
                                     this.props.blogs[3] ?
-                                        <Grid.Column width={5}>
-                                            <Header color='green' as='h3'>
-                                                {this.props.blogs[3].title.split(' ').join(' ')}
-                                            </Header>
-                                            <p>By: Author: {this.props.blogs[3].author}</p>
-                                            <p>{this.props.blogs[3].about}</p>
-                                            <Button
-                                                className="redMoreButton"
-                                                ref={this.props.blogs[3]._id}
-                                                onClick={() => { this.props.onReadMore(this.props.blogs[3]) }}
-                                                name="all"
-                                                style={{color:'blue',backgroundColor:'transparent',border:'none'}}
-                                            >
-                                                <span>Read</span>
-                                            </Button>
-                                        </Grid.Column> : null
+                                        <GridBlog
+                                            onReadMore = {this.props.onReadMore}
+                                            color={this.props.color}
+                                            blog={this.props.blogs[3]}
+                                        />:null
                                 }
                                 {
                                     this.props.blogs[4] ?
-                                        <Grid.Column width={5}>
-                                            <Header color='green' as='h3'>
-                                                {this.props.blogs[4].title.split(' ').join(' ')}
-                                            </Header>
-                                            <p>By: Author: {this.props.blogs[4].author}</p>
-                                            <p>{this.props.blogs[4].about}</p>
-                                            <Button
-                                                className="redMoreButton"
-                                                ref={this.props.blogs[4]._id}
-                                                onClick={() => { this.props.onReadMore(this.props.blogs[4]) }}
-                                                name="all"
-                                                style={{color:'blue',backgroundColor:'transparent',border:'none'}}
-                                            >
-                                                <span>Read</span>
-                                            </Button>
-                                        </Grid.Column> : null
+                                        <GridBlog
+                                            onReadMore = {this.props.onReadMore}
+                                            color={this.props.color}
+                                            blog={this.props.blogs[4]}
+                                        />:null
                                 }
                                 {
                                     this.props.blogs[5] ?
-                                        <Grid.Column width={5}>
-                                            <Header color='green' as='h3'>
-                                                {this.props.blogs[5].title.split(' ').join(' ')}
-                                            </Header>
-                                            <p>By: Author: {this.props.blogs[5].author}</p>
-                                            <p>{this.props.blogs[5].about}</p>
-                                            <Button
-                                                className="redMoreButton"
-                                                ref={this.props.blogs[5]._id}
-                                                onClick={() => { this.props.onReadMore(this.props.blogs[5]) }}
-                                                name="all"
-                                                style={{color:'blue',backgroundColor:'transparent',border:'none'}}
-                                            >
-                                                <span>Read</span>
-                                            </Button>
-                                        </Grid.Column> : null
+                                        <GridBlog
+                                            onReadMore = {this.props.onReadMore}
+                                            color={this.props.color}
+                                            blog={this.props.blogs[5]}
+                                        />:null
+                                }
+                            </Grid>
+                            <Grid>
+                                {
+                                    this.props.blogs[6] ?
+                                        <GridBlog
+                                            onReadMore = {this.props.onReadMore}
+                                            color={this.props.color}
+                                            blog={this.props.blogs[6]}
+                                        />:null
+                                }
+                                {
+                                    this.props.blogs[7] ?
+                                        <GridBlog
+                                            onReadMore = {this.props.onReadMore}
+                                            color={this.props.color}
+                                            blog={this.props.blogs[7]}
+                                        />:null
+                                }
+                                {
+                                    this.props.blogs[8] ?
+                                        <GridBlog
+                                            onReadMore = {this.props.onReadMore}
+                                            color={this.props.color}
+                                            blog={this.props.blogs[8]}
+                                        />:null
+                                }
+                            </Grid>
+                            <Grid>
+                                {
+                                    this.props.blogs[9] ?
+                                        <GridBlog
+                                            onReadMore = {this.props.onReadMore}
+                                            color={this.props.color}
+                                            blog={this.props.blogs[9]}
+                                        />:null
                                 }
                             </Grid>
                         </div> :
