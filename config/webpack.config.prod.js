7bda6778ea8ee74a596684dbba3dd21a56a5f69a
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-12-11 08:44:23 +0300

    build 2017-12-11.

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,5 +1,5 @@
 'use strict';
-const buildID='2017-12-09.';
+const buildID='2017-12-11.';
 const autoprefixer = require('autoprefixer');
 const path = require('path');
 const webpack = require('webpack');
