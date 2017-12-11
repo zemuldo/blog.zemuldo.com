bb8d8935406278376332b7df2fcb154210f026ea
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-12-11 09:51:15 +0300

    swr

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,5 +1,5 @@
 'use strict';
-const buildID='2017-12-00.';
+const buildID='2017-12-11.';
 const autoprefixer = require('autoprefixer');
 const path = require('path');
 const webpack = require('webpack');
