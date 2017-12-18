62547edb49f94877482b2c11f25083f14a477924
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-12-18 18:47:04 +0300

    build 2017-12-18

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,5 +1,5 @@
 'use strict';
-const buildID='2017-12-14';
+const buildID='2017-12-18';
 const autoprefixer = require('autoprefixer');
 const path = require('path');
 const webpack = require('webpack');
--- public/sw.js
+++ public/sw.js
@@ -10,7 +10,7 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 */
-const buildID='2017-12-14';
+const buildID='2017-12-18';
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
