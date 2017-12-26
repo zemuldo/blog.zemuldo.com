270391b9dbdacd1e3a7d5cd799dca20ef442c2f2
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-12-26 23:40:31 +0300

    devel changes testing websocket

--- zemuldo.com.js
+++ zemuldo.com.js
@@ -1,6 +1,6 @@
 let express = require('express');
-const spdy = require('spdy')
-const fs = require('fs')
+const spdy = require('spdy');
+const fs = require('fs');
 let path = require('path');
 let  app = express();
 let bodyParser = require('body-parser');
@@ -12,9 +12,9 @@ const options = {
 };
 let ENV = require('./config/env');
 let env = ENV().raw.NODE_ENV
-let {getBlogTemplate} = require('./tools/tools')
-const conf = require('./src/environments/conf')
-let {getFilterBlogs,getAllBlogs,getBlog,getBlogs} = require('./tools/database')
+let {getBlogTemplate} = require('./tools/tools');
+const conf = require('./src/environments/conf');
+let {getBlog} = require('./db/database');
 const pages = {
     dev:{
         title:"ZemuldO.COM-DEVELOPMENT",
@@ -55,22 +55,26 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
-app.use(helmet())
-app.set('x-powered-by',false)
-app.set('X-Powered-By',false)
-app.use(helmet.ieNoOpen())
-app.use(helmet.xssFilter())
-app.use(helmet.noSniff())
+app.use(helmet());
+app.set('x-powered-by',false);
+app.set('X-Powered-By',false);
+app.use(helmet.ieNoOpen());
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
 app.use(helmet({
     frameguard: false,
     noCache:true
-}))
+}));
+app.use(function(req, res, next) {
+    console.log(req.url)
+    next();
+});
 
 app.get('/sw.js', function(req, res) {
     res.sendFile(path.join(__dirname, 'public', 'sw.js'));
 });
 app.get('/manifest.json', function(req, res) {
-    res.sendFile(path.join(__dirname, 'public', '/manifest.json'));
+    res.sendFile(path.join(__dirname, 'public', 'manifest.json'));
 });
 app.get('*.js', function(req, res, next) {
     req.url = req.url + '.gz';
@@ -84,7 +88,7 @@ app.get('*.css', function(req, res, next) {
     res.set('Content-Type', 'text/css');
     next();
 });
-let expiryDate = new Date(Date.now() + 60 * 60 * 1000) // 1 hour
+let expiryDate = new Date(Date.now() + 60 * 60 * 1000);
 app.use(checkMe({
     name: 'session',
     keys: ['themati#@tripple26=n26gohb()@#$$#$THF$%^$FGDFRFU', '#$THF$%^$FGDFRFU26gohb()@#i#@tripple26='],
@@ -110,7 +114,7 @@ app.use(function(req, res, next) {
     next();
 });
 app.get("/*", async function (req, res) {
-    let url = req.url.split('/').join('')
+    let url = req.url.split('/').join('');
     let incomingPath = 'home'
     if(url.indexOf('-')!==-1){
         incomingPath = url.split('-').join(' ')
@@ -131,7 +135,7 @@ app.get("/*", async function (req, res) {
         }
     }
     else{
-        let blog = await getBlog(incomingPath)
+        let blog = await getBlog(incomingPath);
         if(blog){
             details = {
                 title:blog.title,
@@ -147,7 +151,7 @@ app.get("/*", async function (req, res) {
             }
         }
     }
-    let botPattern = "(googlebot\/|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)";
+    let botPattern = "(googlebot/|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)";
     let re = new RegExp(botPattern, 'i');
     if (re.test(res.locals.ua)) {
         let html = getBlogTemplate(details)
@@ -171,4 +175,4 @@ spdy
         } else {
             console.log('Listening on port: ' + conf[env].httpPort + '.')
         }
-    })
+    });
