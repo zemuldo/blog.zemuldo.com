f92ef558db7c891cb3c5d2d0d420d9de5893728f
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2018-01-06 11:02:23 +0300

    adds more info to topic

--- src/posts/gridBlog.js
+++ src/posts/gridBlog.js
@@ -1,17 +1,41 @@
 import React from 'react'
 import {Grid, Button, Header} from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
+import {topicsOBJ} from '../environments/conf'
+
+const getTopiINfo = (topics)=>{
+    let info = []
+    topics.forEach(function (topic) {
+        info.push(topicsOBJ[topic].full)
+    })
+    return info
+}
 
 class GridBlog extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            showInfo:false
+        }
+        this.someHandler=this.someHandler.bind(this)
+        this.someOtherHandler=this.someOtherHandler.bind(this)
+    }
+
+    someHandler(o) {
+        this.setState({showInfo:true})
+    }
+
+    someOtherHandler(o) {
+        this.setState({showInfo:false})
     }
 
     render() {
         let o = this.props.blog
         return (
-            <Grid.Column width={5}>
+            <Grid.Column
+                width={5}
+                onMouseEnter={() => this.someHandler(o)}
+                onMouseLeave={() => this.someOtherHandler(o)}>
                 <Header color='green' as='h3'>
                     {o.title.split(' ').join(' ')}
                 </Header>
@@ -24,6 +48,19 @@ class GridBlog extends React.Component {
                 </span>
                 <hr color={this.props.color}/>
                 <p>{o.about}</p>
+                {
+                    this.state.showInfo?
+                        <p>
+                            15 Minutes read,
+                            <br/>
+                            Related to
+                            <br/>
+                            {getTopiINfo(o.topics).join(',')}
+                            <br/>
+                            Published on {o.date}
+                        </p>:
+                        null
+                }
                 <Link
                     to={'/' + o.type + '/' + o.topics[0] + '/' + o.userName + '_' + o.title.split(' ').join('-') + '_' + o.date.split(' ').join('-') + '_' + o.id.toString()}>
                     <Button
@@ -33,7 +70,7 @@ class GridBlog extends React.Component {
                             this.props.onReadMore(o)
                         }}
                         name="all"
-                        style={{color: 'blue', backgroundColor: 'transparent', border: 'none'}}
+                        style={{color: 'blue', border: 'none'}}
                     >
                         <span>Read</span>
                     </Button>
