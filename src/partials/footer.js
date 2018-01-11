5748a8727da33d83849ead9709cc286eed29eb61
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2018-01-11 08:46:13 +0300

    linting

--- src/partials/footer.js
+++ src/partials/footer.js
@@ -63,29 +63,29 @@ class Footer extends React.Component {
                   <Header color='green' as='h4' content='Socila Profiles' />
                   <List link>
                     <List.Item>
-                          <a href='https://github.com/zemuldo' rel='noreferrer noopener'
-                            target='_blank'>
-                            <Icon color='black' name='github' />
-                            <span style={{color: 'black'}}
-                                color={this.props.vars.color}>GitHub</span>
-                          </a>
-                        </List.Item>
+                      <a href='https://github.com/zemuldo' rel='noreferrer noopener'
+                        target='_blank'>
+                        <Icon color='black' name='github' />
+                        <span style={{color: 'black'}}
+                          color={this.props.vars.color}>GitHub</span>
+                      </a>
+                    </List.Item>
                     <List.Item>
-                          <a href='https://twitter.com/zemuldo' rel='noreferrer noopener'
-                            target='_blank'>
-                            <Icon color='blue' name='twitter' />
-                            <span style={{color: 'blue'}}
-                                color={this.props.vars.color}>Twitter</span>
-                          </a>
-                        </List.Item>
+                      <a href='https://twitter.com/zemuldo' rel='noreferrer noopener'
+                        target='_blank'>
+                        <Icon color='blue' name='twitter' />
+                        <span style={{color: 'blue'}}
+                          color={this.props.vars.color}>Twitter</span>
+                      </a>
+                    </List.Item>
                     <List.Item>
-                          <a href='https://facebook.com/zemuldo' rel='noreferrer noopener'
-                            target='_blank'>
-                            <Icon color='blue' name='facebook' />
-                            <span style={{color: 'blue'}}
-                                color={this.props.vars.color}>FaceBook</span>
-                          </a>
-                        </List.Item>
+                      <a href='https://facebook.com/zemuldo' rel='noreferrer noopener'
+                        target='_blank'>
+                        <Icon color='blue' name='facebook' />
+                        <span style={{color: 'blue'}}
+                          color={this.props.vars.color}>FaceBook</span>
+                      </a>
+                    </List.Item>
                   </List>
                 </Grid.Column>
                 <Grid.Column width={3}>
@@ -97,67 +97,67 @@ class Footer extends React.Component {
                   <Header color='green' inverted as='h4' content='Navigation' />
                   <List link>
                     <List.Item>
-                          <Link to='/' onClick={() => this.handleHomeClick()}>
-                            <Icon color='blue' name='home' />
-                            <span style={{color: this.props.vars.colors[1]}}
-                                color={this.props.vars.color}>Home Page</span>
-                          </Link>
-                        </List.Item>
+                      <Link to='/' onClick={() => this.handleHomeClick()}>
+                        <Icon color='blue' name='home' />
+                        <span style={{color: this.props.vars.colors[1]}}
+                          color={this.props.vars.color}>Home Page</span>
+                      </Link>
+                    </List.Item>
                     <List.Item>
-                          <Link to={'/tutorials/' + urlDetails}
-                            onClick={() => this.handleMenuItemClick('tuts')}>
-                            <Icon color='blue' name='code' />
-                            <span style={{color: this.props.vars.colors[2]}}
-                                color={this.props.vars.color}>Tutorials</span>
-                          </Link>
-                        </List.Item>
+                      <Link to={'/tutorials/' + urlDetails}
+                        onClick={() => this.handleMenuItemClick('tuts')}>
+                        <Icon color='blue' name='code' />
+                        <span style={{color: this.props.vars.colors[2]}}
+                          color={this.props.vars.color}>Tutorials</span>
+                      </Link>
+                    </List.Item>
                     <List.Item>
-                          <Link to={'/dev/' + urlDetails}
-                            onClick={() => this.handleMenuItemClick('dev')}>
-                            <Icon color='blue' name='code' />
-                            <span style={{color: this.props.vars.colors[3]}}
-                                color={this.props.vars.color}>Development</span>
-                          </Link>
-                        </List.Item>
+                      <Link to={'/dev/' + urlDetails}
+                        onClick={() => this.handleMenuItemClick('dev')}>
+                        <Icon color='blue' name='code' />
+                        <span style={{color: this.props.vars.colors[3]}}
+                          color={this.props.vars.color}>Development</span>
+                      </Link>
+                    </List.Item>
                     <List.Item>
-                          <Link to={'/tech/' + urlDetails}
-                            onClick={() => this.handleMenuItemClick('tech')}>
-                            <Icon color='blue' name='server' />
-                            <span style={{color: this.props.vars.colors[0]}}
-                                color={this.props.vars.color}>Tech Articles</span>
-                          </Link>
-                        </List.Item>
+                      <Link to={'/tech/' + urlDetails}
+                        onClick={() => this.handleMenuItemClick('tech')}>
+                        <Icon color='blue' name='server' />
+                        <span style={{color: this.props.vars.colors[0]}}
+                          color={this.props.vars.color}>Tech Articles</span>
+                      </Link>
+                    </List.Item>
                     <List.Item>
-                          <Link to={'/business/' + urlDetails}
-                            onClick={() => this.handleMenuItemClick('business')}>
-                            <Icon color='blue' name='book' />
-                            <span style={{color: this.props.vars.colors[4]}}
-                                color={this.props.vars.color}>Business Articles</span>
-                          </Link>
-                        </List.Item>
+                      <Link to={'/business/' + urlDetails}
+                        onClick={() => this.handleMenuItemClick('business')}>
+                        <Icon color='blue' name='book' />
+                        <span style={{color: this.props.vars.colors[4]}}
+                          color={this.props.vars.color}>Business Articles</span>
+                      </Link>
+                    </List.Item>
                     <List.Item>
-                          <Link to={'/reviews/' + urlDetails}
-                            onClick={() => this.handleMenuItemClick('reviews')}>
-                            <Icon color='blue' name='star' />
-                            <span style={{color: this.props.vars.colors[3]}}
-                                color={this.props.vars.color}>Reviews</span>
-                          </Link>
-                        </List.Item>
+                      <Link to={'/reviews/' + urlDetails}
+                        onClick={() => this.handleMenuItemClick('reviews')}>
+                        <Icon color='blue' name='star' />
+                        <span style={{color: this.props.vars.colors[3]}}
+                          color={this.props.vars.color}>Reviews</span>
+                      </Link>
+                    </List.Item>
                     <List.Item>
-                          <Link to={'/about/'} onClick={() => this.handleMenuItemClick('about')}>
-                            <Icon color='blue' name='info' />
-                            <span style={{color: this.props.vars.colors[0]}}
-                                color={this.props.vars.color}>About</span>
-                          </Link>
-                        </List.Item>
+                      <Link to={'/about/'} onClick={() => this.handleMenuItemClick('about')}>
+                        <Icon color='blue' name='info' />
+                        <span style={{color: this.props.vars.colors[0]}}
+                          color={this.props.vars.color}>About</span>
+                      </Link>
+                    </List.Item>
                     <List.Item>
-                          <a href='https://danstan.zemuldo.com' rel='noreferrer noopener'
-                            target='_blank'>
-                            <Icon color='blue' name='user' />
-                            <span style={{color: this.props.vars.colors[4]}}
-                                color={this.props.vars.color}>The Creator</span>
-                          </a>
-                        </List.Item>
+                      <a href='https://danstan.zemuldo.com' rel='noreferrer noopener'
+                        target='_blank'>
+                        <Icon color='blue' name='user' />
+                        <span style={{color: this.props.vars.colors[4]}}
+                          color={this.props.vars.color}>The Creator</span>
+                      </a>
+                    </List.Item>
                   </List>
                 </Grid.Column>
               </Grid.Row>
@@ -185,10 +185,10 @@ class Footer extends React.Component {
                 <Modal
                   trigger={
                     <Menu.Item name='Cookie Policy' onClick={this.handleCookieOpen}>
-                          <Icon color={this.props.vars.color} name='privacy' />
-                          <span style={{color: this.props.vars.colors[5]}}
-                            color={this.props.vars.color}>Cookie Policy</span>
-                        </Menu.Item>
+                      <Icon color={this.props.vars.color} name='privacy' />
+                      <span style={{color: this.props.vars.colors[5]}}
+                        color={this.props.vars.color}>Cookie Policy</span>
+                    </Menu.Item>
                                     }
                   open={this.state.cookieModalOpen}
                   onClose={this.handleCookieClose}
@@ -205,11 +205,11 @@ class Footer extends React.Component {
                                               <span style={{color: this.props.vars.colors[2]}}
                                                 color={this.props.vars.color}>Zemuldo.com Cookie Policy</span>
                                             </a>
-                        </p>
+                    </p>
                   </Modal.Content>
                   <Modal.Actions>
                     <Button color={this.props.vars.color} onClick={this.handleCookieClose} inverted>
-                          <Icon name='checkmark' /> OK
+                      <Icon name='checkmark' /> OK
                                         </Button>
                   </Modal.Actions>
                 </Modal>
@@ -218,10 +218,10 @@ class Footer extends React.Component {
                 <Modal
                   trigger={
                     <Menu.Item name='Terms and Conditions' onClick={this.handleUlerOpen}>
-                          <Icon color={this.props.vars.color} name='law' />
-                          <span style={{color: this.props.vars.colors[3]}}
-                            color={this.props.vars.color}>Terms and Conditions</span>
-                        </Menu.Item>
+                      <Icon color={this.props.vars.color} name='law' />
+                      <span style={{color: this.props.vars.colors[3]}}
+                        color={this.props.vars.color}>Terms and Conditions</span>
+                    </Menu.Item>
                                     }
                   open={this.state.ulerModalaOpen}
                   onClose={this.handleUlerClose}
@@ -238,11 +238,11 @@ class Footer extends React.Component {
                                               <span
                                                 color={this.props.vars.color}> Zemuldo.COM Terms and Conditions</span>
                                             </a>
-                        </p>
+                    </p>
                   </Modal.Content>
                   <Modal.Actions>
                     <Button color={this.props.vars.color} onClick={this.handleUlerClose} inverted>
-                          <Icon name='checkmark' /> OK
+                      <Icon name='checkmark' /> OK
                                         </Button>
                   </Modal.Actions>
                 </Modal>
