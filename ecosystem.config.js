06d90a3ddd7f0a6ffdc3d386f417b3347be78e27
Danstan Onyango <danstan.otieno@gmail.com>
2019-05-17 00:16:46 +0300

    adds pm2 file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [
         {
-          name: "Server 0 8070",
+          name: "Blog Server 0 8070",
           script: "./app.js",
           watch: true,
           env: {
@@ -12,58 +12,6 @@ module.exports = {
               "PORT": 8070,
               "NODE_ENV": "production",
           }
-        },
-        {
-            name: "Server 1 8071",
-            script: "./app.js",
-            watch: true,
-            env: {
-                "PORT": 8071,
-                "NODE_ENV": "development"
-            },
-            env_production: {
-                "PORT": 8071,
-                "NODE_ENV": "production",
-            }
-        },
-        {
-            name: "Server 2 8072",
-            script: "./app.js",
-            watch: true,
-            env: {
-                "PORT": 8072,
-                "NODE_ENV": "development"
-            },
-            env_production: {
-                "PORT": 8072,
-                "NODE_ENV": "production",
-            }
-        },
-        {
-            name: "Server 3 8073",
-            script: "./app.js",
-            watch: true,
-            env: {
-                "PORT": 8073,
-                "NODE_ENV": "development"
-            },
-            env_production: {
-                "PORT": 8073,
-                "NODE_ENV": "production",
-            }
-        },
-        {
-            name: "Server 4 8074",
-            script: "./app.js",
-            watch: true,
-            env: {
-                "PORT": 8074,
-                "NODE_ENV": "development"
-            },
-            env_production: {
-                "PORT": 8074,
-                "NODE_ENV": "production",
-            }
         }
     ]
   }
